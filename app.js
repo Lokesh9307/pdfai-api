a068f728263cc1c9c9580a0c735ff8007d5659be
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-07-16 14:31:53 +0530

    port binding error

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/api/chat', chatRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
